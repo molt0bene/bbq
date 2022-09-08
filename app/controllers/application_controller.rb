8b054f6d185ffa0ece8013d773141ed2f1d11c1c
molt0bene <alisar3@yandex.ru>
2022-09-08 20:48:28 +0300

    added: user can't subscribe to own event

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user_can_edit?
+  helper_method :this_is_current_user?
 
   def current_user_can_edit?(model)
     user_signed_in? && (
       model.user == current_user ||
         (model.try(:event).present? && model.event.user == current_user)
     )
+  end
+
+  def this_is_current_user?(model)
+    user_signed_in? && model.user == current_user
   end
 end
