5e3e19046045fd2c1c32ff8fa271296577debff8
molt0bene <alisar3@yandex.ru>
2022-09-16 15:28:21 +0300

    subscriptions bug fixed

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,7 +3,6 @@ class Subscription < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :event, presence: true
-  validates :photo, presence: true
 
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
