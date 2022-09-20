4f405cec8922a20af63f9c2e5cd837a72d98236d
molt0bene <alisar3@yandex.ru>
2022-09-20 09:09:20 +0300

    gitignore updated for vps

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   end
 
   def fa_icon(icon_class)
-    content_tag 'span', '', class: "fa fa-#{icon_class}"
+    content_tag 'span', '', class: "far fa-#{icon_class}"
   end
 
   def event_photo(event)
