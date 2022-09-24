88fa8d02e56eeba6574e3c4236fdb12b5f8ff32a
molt0bene <alisar3@yandex.ru>
2022-09-24 11:18:33 +0300

    mailjet module added

--- config/application.rb
+++ config/application.rb
@@ -41,3 +41,11 @@ module Bbq
     config.generators.system_tests = nil
   end
 end
+
+module Mailjet
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    config.generators.system_tests = nil
+    config.action_mailer.delivery_method = :mailjet
+  end
+end
