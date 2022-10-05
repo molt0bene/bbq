73bbd954b0c859f17dd803749dc727d17b365233
molt0bene <alisar3@yandex.ru>
2022-10-05 23:22:50 +0300

    map pt.2

--- app/assets/builds/map.js
+++ app/assets/builds/map.js
@@ -3,29 +3,31 @@
   ymaps.ready(init);
   var myMap;
   function init() {
-    address = document.getElementById("map").getAttribute("data-address");
-    myMap = new ymaps.Map("map", {
-      center: [55.76, 37.64],
-      zoom: 10
+    document.addEventListener("turbo:load", () => {
+      address = document.getElementById("map").getAttribute("data-address");
+      myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 10
+      });
+      myGeocoder = ymaps.geocode(address);
+      myGeocoder.then(
+        function(res) {
+          coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+          myMap.geoObjects.add(
+            new ymaps.Placemark(
+              coordinates,
+              { iconContent: address },
+              { preset: "islands#blueStretchyIcon" }
+            )
+          );
+          myMap.setCenter(coordinates);
+          myMap.setZoom(15);
+        },
+        function(err) {
+          alert("error");
+        }
+      );
     });
-    myGeocoder = ymaps.geocode(address);
-    myGeocoder.then(
-      function(res) {
-        coordinates = res.geoObjects.get(0).geometry.getCoordinates();
-        myMap.geoObjects.add(
-          new ymaps.Placemark(
-            coordinates,
-            { iconContent: address },
-            { preset: "islands#blueStretchyIcon" }
-          )
-        );
-        myMap.setCenter(coordinates);
-        myMap.setZoom(15);
-      },
-      function(err) {
-        alert("error");
-      }
-    );
   }
 })();
 //# sourceMappingURL=assets/map.js.map
