9f9d3b021fd6a99093036e2cdc0ea4ee12924dd7
molt0bene <alisar3@yandex.ru>
2022-10-05 23:26:13 +0300

    minor fix for capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'moltobenedeploy.ru', user: 'deploy', roles: %w{app db web}
+server 'moltobenedeploy.ru', user: 'deploy', roles: %w[app db web]
 
 # role-based syntax
 # ==================
